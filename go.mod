8c6f15e4e1631b70c246d107b2c9f528a3d230bd
Adonnis Peralta <adonnis.peralta@gmail.com>
2025-03-27 22:09:07 -0500

    Create initDB() and shortenerHandler()

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/monkeyman718/url-shortener-v2
 
 go 1.24.1
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    "log"
+    "net/http"
+    "math/rand"
+
+    _ "github.com/lib/pq"
+    "github.com/gorilla/mux"
+)
+
+const shortURLLength = 6
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+
+func main(){
+    initDB()
+
+    router := mux.NewRouter()
+    router.HandleFunc("/shorten", shortenerHandler )
+
+    fmt.Println("Listening on port 8080...")
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func initDB(){
+    sqlStr := "host=localhost port=5432 user=postgres password=postgres sslmode=disable"    
+    // Open psql connection
+    sqlconn, err := sql.Open("postgres", sqlStr)
+
+    if err != nil {
+        panic(err)
+        return
+    }
+
+    // Close psql connection with defer
+    defer sqlconn.Close()
+
+    err = sqlconn.Ping()
+    
+    // Check for errors connecting
+    if err != nil {
+        fmt.Printf("Cannot connect to database: %v", err)
+        return 
+    }
+    
+    fmt.Println("Connection to database successful!")
+}
+
+func generateShortURL() string {
+    shortCode := make([]byte, shortURLLength)
+    for i := 0; i < shortURLLength; i++ {
+        shortCode[i] = charset[rand.Intn(shortURLLength)]
+    }
+
+    return string(shortCode)
+}
+
+func shortenerHandler(w http.ResponseWriter, r *http.Request){
+    shortCode := generateShortURL() 
+    //longURL := r.URL.Query().Get("url")
+
+    fmt.Fprintf(w, "http://localhost:8080/%v\n", shortCode)
+}
+//func redirectHandler(w http.ResponseWriter, r *http.Response){}
